6952ae43db182e3547ed3b118cf407ef5c3bdc54
Denys <denis.sidorenko3001@gmail.com>
2024-08-13 18:34:08 +0300

    add-styles

--- SixDaysScreen.tsx
+++ SixDaysScreen.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import {View, Text, StyleSheet, ActivityIndicator, FlatList, Alert} from 'react-native';
+import {Image,View, Text, StyleSheet, ActivityIndicator, FlatList, Alert} from 'react-native';
 import axios from 'axios';
 import { fetchWeatherData } from './fetchWeather';
+import { getWeatherImage } from './getWeatherImage';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const API_KEY_WEATHER = 'cbf8b8330b7cb5b3f1e535563cba25bc';
 
@@ -10,12 +13,11 @@ export default function SixDaysForecast({route}: any) {
     const [forecastData, setForecastData] = useState<any>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-    const [searchHistory, setSearchHistory] = useState<string[]>([]);
 
 
     useEffect(() => {
         fetchForecastData();
-        Alert.alert('CITY',city)
+        // Alert.alert('CITY',city)
 
     },[city]);
 
@@ -34,7 +36,6 @@ export default function SixDaysForecast({route}: any) {
             setLoading(false);
         }
     }
-    console.log(forecastData);
     if(loading){
         <View style={styles.container}>
             <Text> Error: {error}</Text>
@@ -55,25 +56,33 @@ export default function SixDaysForecast({route}: any) {
     .map((item:any) => ({
         date: new Date(item.dt * 1000).toLocaleDateString(),
         temp: item.main.temp,
-        description: item.weather[0].description
+        description: item.weather[0].description,
+        image: getWeatherImage(item.weather[0].description)
     }));
     
 
         return (
-            <View style={styles.container}>
-            <Text>5-Day Weather Forecast for {city}</Text>
+         
+            <LinearGradient
+            colors={['#03c2fc', '#61ffba']}
+            style={styles.container}
+        >
+            <Text  style={styles.title}>5-Day Weather Forecast for {city}</Text>
             <FlatList
                 data={dailyForecastArray}
                 keyExtractor={(item) => item.date}
                 renderItem={({ item }) => (
                     <View style={styles.dayContainer}>
-                        <Text>Date: {item.date}</Text>
-                        <Text>Temperature: {item.temp.toFixed(1)} °C</Text>
-                        <Text>Description: {item.description}</Text>
+                   
+                    <Image source={{ uri: item.image }} style={styles.image}></Image>
+                    <Text  style={styles.date}>Date: {item.date}</Text>
+                    <Text >Temperature: 
+                        <Text style={styles.temperature}>{item.temp.toFixed(1)} °C</Text></Text>
+                    <Text>Description: {item.description}</Text>
                     </View>
                 )}
-            />
-        </View>
+                />
+        </LinearGradient>
         )
     
 
@@ -85,10 +94,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 24,
     },
+    temperature: {
+        fontWeight: '500',
+        fontSize: 18,
+    },
+    date: {
+        fontWeight: '500',
+        fontSize: 16,
+    },
     dayContainer: {
         padding: 16,
         borderBottomWidth: 1,
         borderBottomColor: 'gray',
         width: '100%',
+    },
+    description: {
+        width:170,
+        height:70,
+    },
+    title: {
+        fontWeight: '500',
+        fontSize: 25,
+    },
+    image: {
+        width: 170,
+        height: 70,
+        borderRadius: 10,
+        marginBottom: 10,
+        borderWidth: 2,
+        borderColor: '#ffffff', 
     },
 });
