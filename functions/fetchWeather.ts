1f7281e09e0bbcb48694ea79cbf324a622665275
Denys <denis.sidorenko3001@gmail.com>
2024-08-21 18:44:28 +0300

    changesLocation

--- functions/fetchWeather.ts
+++ functions/fetchWeather.ts
@@ -9,25 +9,40 @@ export const fetchWeatherData = async (
   setWeatherData: (data: any) => void,
   setLoading: (loading: boolean) => void,
   setError: (error: string | null) => void,
-  setSearchHistory: (history: string[]) => void
+  setSearchHistory: (history: string[]) => void,
+  latitude?: number,
+  longitude?: number,
+  
 ) => {
-  if (!city.trim()) {
-    setError('Please enter a city');
+  if (!city.trim() && (!latitude || !longitude)) {
+    setError('Please enter a city and provides location');
     setLoading(false);
     return null;
   }
 
+  
   try {
     setLoading(true);
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY_WEATHER}&units=metric`
-    );
+
+    let response;
+    if (latitude && longitude) {
+      response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY_WEATHER}&units=metric`
+      );
+    } else {
+      response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY_WEATHER}&units=metric`
+      );
+    }
+
     const weatherData = response.data;
     setWeatherData(weatherData);
 
-    const updatedHistory = [city, ...searchHistory.filter((item) => item !== city)];
-    setSearchHistory(updatedHistory);
-    await AsyncStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+    if (city.trim()) {
+      const updatedHistory = [city, ...searchHistory.filter((item) => item !== city)];
+      setSearchHistory(updatedHistory);
+      await AsyncStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+    }
 
     setError(null);
 
--- screens/WeatherMap.tsx
+++ screens/WeatherMap.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View,Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useLocation } from '../context/LocationContext';
+import axios from 'axios';
+import { fetchWeatherData } from '../functions/fetchWeather';
 
 const WeatherMapLocation = ({ route }: any) => {
   const { location } = useLocation();
-
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading,setLoading] = useState<boolean>(false);
   if (!location) {
     return <View><Text>No location available</Text></View>;
   }
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      if( location) {
+        try {
+          setLoading(true);
+          const data = await fetchWeatherData(
+            '', 
+            [], 
+            setWeatherData, 
+            setLoading, 
+            setError, 
+            (history) => {}, 
+            location.latitude, 
+            location.longitude
+          );          setWeatherData(data);
+        }catch (e) {
+          setError(`Eror ${e}`)
+        }
+        finally {
+          setLoading
+        }
+    }
+    }
+    fetchWeather();
+  },[location])
+  if (!location) {
+    return <View><Text>No location available</Text></View>;
+  }
+
+  if (loading) {
+    return <View><Text>Loading weather information...</Text></View>;
+  }
+
+  if (error) {
+    return <View><Text>{error}</Text></View>;
+  }
+
 
 
   return (
@@ -25,9 +67,10 @@ const WeatherMapLocation = ({ route }: any) => {
         <Marker
           coordinate={{ latitude: location.latitude, longitude: location.longitude }}
           title="Your Location"
-          description="Weather information here"
+          description={`Weather in ${weatherData?.name}, Temp :${weatherData?.main?.temp} °C`}
         />
       </MapView>
+      <Text>{weatherData?.weather[0]?.name}</Text>
     </View>
   );
 };
